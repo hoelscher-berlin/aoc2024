9dbb3839f5276218dfd23b89566dbc01569ccb0e
Simon Hölscher <hoelscher@iserv.kkg.berlin>
2024-12-10 09:30:52 +0100

    part 2 :)

--- 10/10.rb
+++ 10/10.rb
@@ -24,42 +24,36 @@ def inbound(c)
     return c[0] < $ylen && c[0] >=0 && c[1] < $xlen && c[1] >=0
 end
 
-def find_peaks(grid, c, found)
+def find_peaks(grid, c, found, nr)
     if grid[c[0]][c[1]] == 9
         if !found.include?([c[0],c[1]])
             found << [c[0],c[1]]
-        end
+        end 
+        #puts "ping"
+        nr << 1
         return
     end
     dirs = [-1,0,1] 
-    #puts "we are currently at "+c.inspect
     dirs.each do |dy|
         dirs.each do |dx|
             ny = c[0]+dy
             nx = c[1]+dx
             nc = [ny,nx]
-            #puts "nc would be"+nc.inspect
             if (dx+dy).abs==1 && inbound(nc) && grid[ny][nx] == grid[c[0]][c[1]] + 1
-                #puts "going to "+nc.inspect
-                find_peaks(grid, nc, found)
+                find_peaks(grid, nc, found, nr)
             end
         end
     end
 end
 
-#puts trailheads.inspect
-#puts
-
 trailheads.each do |th|
-    #if th != [0,2]
-    #    next
-    #end
-    #puts "checking th "+th[0].to_s+","+th[1].to_s
     res = []
-    find_peaks(t1, th,res)
-    #puts res.inspect
-    #puts res.length
+    nr = []
+    find_peaks(t1, th,res,nr)
     sum1 += res.length
+    sum2 += nr.length
 end
 
+
 puts sum1
+puts sum2
