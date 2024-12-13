af344957be652745f34f8f34142d9537af6da5ad
Simon Hölscher <hoelscher@iserv.kkg.berlin>
2024-12-13 12:17:31 +0100

    matrix ftw

--- 13/13.rb
+++ 13/13.rb
@@ -3,7 +3,7 @@ sum2 = 0
 games = []
 ax = bx = ay = by = x = y = 0
 
-File.open("example.txt").each_with_index do |line,i|
+File.open("input.txt").each_with_index do |line,i|
     if i%4 == 0
         line = line.strip.split("+")
         ax = line[1][0..1].to_i
@@ -21,7 +21,61 @@ File.open("example.txt").each_with_index do |line,i|
     end
 end
 
-puts games.inspect
+#puts games.inspect
+
+games.each do |g|
+    a = g[0]
+    c = g[1]
+    b = g[2]
+    d = g[3]
+    x = g[4]
+    y = g[5]
+
+    divby = (a*d*1.0 - b*c*1.0)
+
+    nx = (x*d-b*y)/divby
+    ny = (-c*x + a*y)/divby
+
+
+    if !(nx <= 100 && ny <= 100 && nx%1==0 && ny%1 == 0)
+        next
+    end
+
+    #puts nx
+    #puts ny
+
+    #puts 3*nx + ny
+    #puts
+
+    sum1+=3*nx + ny
+end
+
+games.each do |g|
+    a = g[0]
+    c = g[1]
+    b = g[2]
+    d = g[3]
+    x = g[4] + 10000000000000
+    y = g[5] + 10000000000000
+
+    divby = (a*d*1.0 - b*c*1.0)
+
+    nx = (x*d-b*y)/divby
+    ny = (-c*x + a*y)/divby
+
+
+    if !(nx%1==0 && ny%1 == 0)
+        next
+    end
+
+    #puts nx
+    #puts ny
+
+    #puts 3*nx + ny
+    #puts
+
+    sum2+=3*nx + ny
+end
 
 puts sum1
 puts sum2
