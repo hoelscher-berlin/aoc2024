a5a808733183f939dcf616ba22a73d89bfeb5c6e
Simon Hölscher <hoelscher@iserv.kkg.berlin>
2024-12-15 12:27:01 +0100

    part 1 and parsing for part 2

--- 15/15.rb
+++ 15/15.rb
@@ -34,5 +34,142 @@ File.open("e1.txt").each do |r|
     end    
 end
 
+ylen = map.length
+xlen = map[0].length
+ry = 0
+rx = 0
+
+map.each_with_index do |row,y|
+    row.each_with_index do |val,x|
+        if val=="@"
+            ry = y
+            rx = x
+        end
+    end
+end
+
+
 prettyprint(map)
-puts moves
+
+moves.each do |m|
+    dy = 0
+    dx = 0
+
+    case m
+    when "^"
+        dy = -1
+    when ">"
+        dx = 1
+    when "v"
+        dy = 1
+    when "<"
+        dx = -1
+    end
+
+    ny = ry + dy
+    nx = rx + dx
+
+    if map[ny][nx] == "#"
+        next
+    elsif map[ny][nx]=="."
+        map[ry][rx] = "."
+        map[ny][nx] = "@"
+        rx = nx
+        ry = ny
+    else
+        # pushing boxes
+        case m
+        when "^"
+            (1..ny-1).to_a.reverse.each do |y|
+                if map[y][rx] == "."
+                    map[ry][rx]="."
+                    map[ny][nx]="@"
+                    map[y][rx]="O"
+                    rx = nx
+                    ry = ny
+                    break
+                elsif map[y][rx] == "#"
+                    break
+                end
+            end 
+        when ">"
+            (nx+1..xlen-2).each do |x|
+                if map[ry][x] == "."
+                    map[ry][rx]="."
+                    map[ny][nx]="@"
+                    map[ry][x]="O"
+                    rx = nx
+                    ry = ny
+                    break
+                elsif map[ry][x] == "#"
+                    break
+                end
+            end
+        when "v"
+            (ny+1..ylen-2).each do |y|
+                if map[y][rx] == "."
+                    map[ry][rx]="."
+                    map[ny][nx]="@"
+                    map[y][rx]="O"
+                    rx = nx
+                    ry = ny
+                    break
+                elsif map[y][rx] == "#"
+                    break
+                end
+            end
+        when "<"
+            (1..nx-1).to_a.reverse.each do |x|
+                if map[ry][x] == "."
+                    map[ry][rx]="."
+                    map[ny][nx]="@"
+                    map[ry][x]="O"
+                    rx = nx
+                    ry = ny
+                    break
+                elsif map[ry][x] == "#"
+                    break
+                end
+            end
+        end
+    end
+end
+
+map.each_with_index do |row,y|
+    row.each_with_index do |v,x|
+        if v == "O"
+            sum1 += 100*y+x
+        end
+    end
+end
+
+prettyprint(map)
+puts sum1
+
+# part 2
+map = []
+
+File.open("input.txt").each do |r|
+    r = r.strip
+    if r == ""
+        break
+    end
+
+    line = []
+
+    r.split(//).each do |v|
+        if v == "." || v == "#"
+            line << v
+            line << v
+        elsif v == "O"
+            line << "["
+            line << "]"
+        else
+            line << "@"
+            line << "."
+        end
+    end
+    map << line
+end
+
+prettyprint(map)
