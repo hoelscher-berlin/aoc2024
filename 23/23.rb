f3a0093112f299bebacb368cad79bae953b77a13
Simon Hölscher <hoelscher@iserv.kkg.berlin>
2024-12-23 14:14:15 +0100

    using part2 for part 1 too, cleanup

--- 23/23.rb
+++ 23/23.rb
@@ -1,7 +1,5 @@
 require 'set'
-require 'benchmark'
 require 'rgl/adjacency'
-require 'rgl/dot'
 file = "input.txt"
 
 sum1 = 0
@@ -20,13 +18,34 @@ pairs.each do |p|
     graph.add_edge(p[0],p[1])
 end
 
-triples = Set.new
+k=2
+cliques = graph.edges.map { |edge| [edge.source, edge.target] }.sort
+triples = []
 
-graph.vertices.combination(3).each do |v|
-    a, b, c = v
-    if graph.has_edge?(a,b) && graph.has_edge?(b,c) && graph.has_edge?(c,a)
-        triples.add(v)
+while true
+    next_cliques = Set.new
+
+    if k == 3
+        triples = cliques
     end
+
+    groups = cliques.group_by { |subarray| subarray[0..-2] }
+    groups.each do |common_prefix, subarrays|
+        next if subarrays.size < 2
+      
+        subarrays.combination(2) do |pair|
+            if graph.has_edge?(pair[0][-1],pair[1][-1])
+                next_cliques.add((pair[0] + [pair[1][-1]]).sort)
+            end
+        end
+    end
+
+    if next_cliques.length == 0
+        break
+    end
+
+    cliques = next_cliques.sort
+    k += 1
 end
 
 good_triples = []
@@ -41,72 +60,6 @@ triples.each do |t|
 end
 
 sum1 = good_triples.length
-
 puts sum1
 
-k=3
-cliques = triples
-cliques = cliques.sort
-
-while true
-    next_cliques = Set.new
-
-    groups = cliques.group_by { |subarray| subarray[0..-2] }
-
-    groups.each do |common_prefix, subarrays|
-        next if subarrays.size < 2
-      
-        subarrays.combination(2) do |pair|
-            if graph.has_edge?(pair[0][-1],pair[1][-1])
-                next_cliques.add(pair[0] + [pair[1][-1]])
-            end
-        end
-    end
-
-    if next_cliques.length == 0
-        break
-    end
-
-    cliques = next_cliques.sort
-    k += 1
-end
-
 puts cliques[0].sort.join(",")
-
-=begin
-
-def bron_kerbosch(graph, r, p, x, cliques)
-    if p.empty? && x.empty?
-        cliques << r
-        return
-    end
-
-    p.dup.each do |v|
-        new_r = r + [v]
-        new_p = p.select { |u| graph.has_edge?(v, u) }
-        new_x = x.select { |u| graph.has_edge?(v, u) }
-
-        bron_kerbosch(graph, new_r, new_p, new_x, cliques)
-
-        p.delete(v)
-        x << v
-    end
-end
-  
-def find_cliques(graph)
-    cliques = []
-    vertices = graph.vertices.to_a
-
-    bron_kerbosch(graph, [], vertices, [], cliques)
-
-    cliques
-end
-
-max_cliques = find_cliques(graph)
-
-max = max_cliques.max_by(&:length)
-
-puts max.sort.join(",")
-
-#graph.write_to_graphic_file('jpg', 'graph_'+file)
-=end
