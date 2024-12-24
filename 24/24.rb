c2633da2546fea74cc71e9a92a3d2d62c095fd51
Simon Hölscher <hoelscher@iserv.kkg.berlin>
2024-12-24 12:53:39 +0100

    had some fun time looking at graphs

--- 24/24.rb
+++ 24/24.rb
@@ -1,5 +1,7 @@
+require 'rgl/adjacency'
+require 'rgl/dot'
 require 'set'
-file = "input.txt"
+file = "input_4.txt"
 
 sum1 = 0
 sum2 = 0
@@ -12,6 +14,8 @@ gates = Queue.new
 x = []
 y = []
 
+graph = RGL::DirectedAdjacencyGraph.new
+j=0
 mode = 0
 File.open(file).each do |r|
     line = r.strip.split(": ")
@@ -20,7 +24,7 @@ File.open(file).each do |r|
         next
     end
 
-    if mode ==0
+    if mode == 0
         val = line[1].to_i
         wires[line[0]] = val
         if line[0][0] == "x"
@@ -32,18 +36,48 @@ File.open(file).each do |r|
         line = r.strip.split(" ")
         gate = {"type" => line[1], "w1" => line[0], "w2" => line[2], "output"=>line[4]}
         gates << gate
+        graph.add_edge(line[0],line[1]+j.to_s)
+        graph.add_edge(line[2],line[1]+j.to_s)
+        graph.add_edge(line[1]+j.to_s,line[4])
+        j+=1
     end
 end
-
+#=begin
 x = x.reverse.join("").to_i(2)
 y = y.reverse.join("").to_i(2)
 expected = x+y
+#=end
 
-puts "x: #{x}"
-puts "y: #{y}"
-puts "expected result: #{expected}"
+# look at it and fix it :)
+#graph.write_to_graphic_file('jpg')
 
+#=begin
+max_45_bit_value = (1 << 45) - 1  # 2^45 - 1
+# Generate two random 45-bit numbers
+xold = Random.rand(0..max_45_bit_value)
+yold = Random.rand(0..max_45_bit_value)
 
+expected = xold+yold
+
+x = xold.to_s(2).split("").reverse
+y = yold.to_s(2).split("").reverse
+
+45.times do |i|
+    nr = i.to_s
+    nr = "0"+i.to_s if i < 10
+    if i<x.length
+        wires["x"+nr] = x[i].to_i
+    else
+        wires["x"+nr] = 0
+    end
+
+    if i<y.length
+        wires["y"+nr] = y[i].to_i
+    else
+        wires["y"+nr] = 0
+    end
+end
+#=end
 
 def calc(w1,w2,op)
     case op
@@ -56,7 +90,6 @@ def calc(w1,w2,op)
     end
 end
 
-
 while !gates.empty?
     g = gates.deq
     if wires.key?(g["w1"]) && wires.key?(g["w2"])
@@ -68,4 +101,4 @@ end
 
 sum1 = wires.select {|w,v| w[0] == "z"}.sort.reverse.map{|v| v[1]}.join.to_i(2)
 puts sum1
-puts (sum1 == expected)
+puts expected == sum1
